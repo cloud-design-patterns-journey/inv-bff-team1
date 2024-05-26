24db838c9ee1b9bfeb19edbd1ae9a7b3fb32bb62
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2024-05-26 17:35:24 +0200

    Adds stock items controller
    Signed-off-by: sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>

---
+++ src/controllers/stock-items.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("stock-items")
+export class StockItemsController {
+  @Get()
+  async listStockItems(): Promise<any[]> {
+    return [];
+  }
+}
